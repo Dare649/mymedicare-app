175c9a052e1e9152e5d7dd056840d0a7923c7bad
Damillare Ajayi <darea649@gmail.com>
2025-06-03 17:11:27 +0100

    updated the sign up

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -67,7 +67,7 @@ const Signup = () => {
     try {
       const response = await dispatch(signUp(formData)).unwrap();
       toast.success(response.message);
-      router.push('/auth/sign-in');
+      router.push('/auth/verify-otp');
     } catch (error: any) {
       toast.error(error.message);
     } finally {
